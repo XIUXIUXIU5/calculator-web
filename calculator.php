cd5a477de18283d58241bc72cf89af1072eec1a3
Lei Shao <shaolei@ucla.edu>
2015-09-30 18:01:30 -0700

    setup & some text

---
+++ calculator.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Calculator</h1>
+<p>Created by Lei Shao and Jing Wu.</p>
+<p>Type an expression in the following box (e.g., 10.5+20*3/25).</p>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  <input type="text" name="expr">
+  <input type="submit" name="Calculate">
+</form>
+
+<ul>
+	<li>Only numbers and +,-,* and / operators are allowed in the expression.</li>
+	<li>The evaluation follows the standard operator precedence.</li>
+	<li>The calculator does not support parentheses.</li>
+	<li>The calculator handles invalid input "gracefully". It does not output PHP error messages.</li>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $expr = $_POST['expr'];
+}
+?>
+
+</body>
+</html>
