b6fd17c986d1e7629d3e04b9c45ebcaf688be2bc
Lei Shao <shaolei@ucla.edu>
2015-09-30 18:16:16 -0700

    change to get

--- calculator.php
+++ calculator.php
@@ -6,7 +6,7 @@
 <p>Created by Lei Shao and Jing Wu.</p>
 <p>Type an expression in the following box (e.g., 10.5+20*3/25).</p>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
   <input type="text" name="expr">
   <input type="submit" name="Calculate">
 </form>
@@ -18,8 +18,9 @@
 	<li>The calculator handles invalid input "gracefully". It does not output PHP error messages.</li>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $expr = $_POST['expr'];
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $expr = $_GET['expr'];
+    var_dump($expr);
 }
 ?>
 
