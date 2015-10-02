0bbca2d7eca1219164875230a92af7447a192cde
jwu0210 <jiwu@media.ucla.edu>
2015-10-02 00:35:09 -0700

    invalid expression check

--- calculator.php
+++ calculator.php
@@ -2,34 +2,49 @@
 <html>
 <body>
 
-<h1>Calculator</h1>
-<p>Created by Lei Shao and Jing Wu.</p>
-<p>Type an expression in the following box (e.g., 10.5+20*3/25).</p>
+  <h1>Calculator</h1>
+  <p>Created by Lei Shao and Jing Wu.</p>
+  <p>Type an expression in the following box (e.g., 10.5+20*3/25).</p>
 
-<form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  <input type="text" name="expr">
-  <input type="submit" name="Calculate">
-</form>
+  <form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <input type="text" name="expr">
+    <input type="submit" value="Calculate">
+  </form>
 
-<ul>
-	<li>Only numbers and +,-,* and / operators are allowed in the expression.</li>
-	<li>The evaluation follows the standard operator precedence.</li>
-	<li>The calculator does not support parentheses.</li>
-	<li>The calculator handles invalid input "gracefully". It does not output PHP error messages.</li>
+  <ul>
+   <li>Only numbers and +,-,* and / operators are allowed in the expression.</li>
+   <li>The evaluation follows the standard operator precedence.</li>
+   <li>The calculator does not support parentheses.</li>
+   <li>The calculator handles invalid input "gracefully". It does not output PHP error messages.</li>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+   <?php
+   if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $expr = $_GET['expr'];
+    echo "<br>";
 
-    //TO DO: check if the expression is valid
-
-    $result = eval('return '.$expr.';');
-    echo "<h2>Result</h2>";
-    if ($result) {
-    	echo $expr.' = '.$result;
+    if (preg_match("/^-?\d+(\.\d+)?([-*\+\/]-?\d+(\.\d+)?)*$/", $expr))
+    {
+      if (preg_match("/\/0/", $expr))
+        echo "Division by zero error!";
+      else
+      {
+      //TO DO: old terms do not vanish
+      //TO DO: 1-1 bug
+      //TO DO: 1+-1 bug
+      //TO DO: what about 0/0
+        echo "Valid expression!";
+        echo "<br>";
+        $result = eval('return '.$expr.';');
+        echo "<h2>Result</h2>";
+        if ($result) {
+         echo $expr.' = '.$result;
+        }
+      }
     }
-}
-?>
+    else
+      echo "Invalid expression!";
+  }
+ ?>
 
 </body>
 </html>
